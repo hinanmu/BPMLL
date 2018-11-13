9d6b8d8ea782d21e791ac3f77547b76530edd744
ustc-nanmu <hinanmu@gmail.com>
2018-11-13 22:44:35 +0800

    update

--- bp_mll_train.py
+++ bp_mll_train.py
@@ -8,30 +8,33 @@ from sklearn.externals import joblib
 from operator import itemgetter
 
 def train(data_x, data_y):
-    batch_size = 32
     data_num = data_x.shape[0]
     feature_num = data_x.shape[1]
     label_num = data_y.shape[1]
+    hidden_unit = int(feature_num * 0.8)
+    alpha = 0.1
+    batch_size = 32
+
     x = tf.placeholder(tf.float32, shape=[None, feature_num], name='input_x')
     y = tf.placeholder(tf.float32, shape=[None, label_num], name='input_y')
 
-    w1 = tf.Variable(tf.random_normal([feature_num, 3], stddev=0.01, seed=1))
-    w2 = tf.Variable(tf.random_normal([3, label_num], stddev=1, seed=1))
+    w1 = tf.Variable(tf.random_normal([feature_num, hidden_unit], stddev=1, seed=1))
+    w2 = tf.Variable(tf.random_normal([hidden_unit, label_num], stddev=1, seed=1))
 
-    bias1 = tf.Variable(tf.random_normal([3], stddev=0.001, seed=1))
-    bias2 = tf.Variable(tf.random_normal([label_num], stddev=0.001, seed=1))
+    bias1 = tf.Variable(tf.random_normal([hidden_unit], stddev=0.01, seed=1))
+    bias2 = tf.Variable(tf.random_normal([label_num], stddev=0.01, seed=1))
 
     a = tf.nn.tanh(tf.matmul(x, w1) + bias1)
     pred = tf.nn.tanh(tf.matmul(a, w2) + bias2)
     tf.add_to_collection('pred_network', pred)
-    loss = loss_fun(y, pred)
+    loss = loss_fun(y, pred) + tf.contrib.layers.l2_regularizer(alpha)(w1) + tf.contrib.layers.l2_regularizer(alpha)(w2)
 
-    optimazer = tf.train.AdamOptimizer(0.0001).minimize(loss)
+    optimazer = tf.train.AdamOptimizer(0.05).minimize(loss)
 
     with tf.Session() as sess:
         init = tf.global_variables_initializer()
         sess.run(init)
-        steps = 5000
+        steps = int(500 * data_num / batch_size)
 
         for i in range(steps):
             start = (i * batch_size) % data_num
@@ -117,11 +120,11 @@ def pairwise_and(a, b):
     row = tf.expand_dims(b, 1)
     return tf.logical_and(column, row)
 
-def load_data():
-    x_train = np.load('dataset/yeast/x_train.npy')
-    y_train = np.load('dataset/yeast/y_train.npy')
-    x_test = np.load('dataset/yeast/x_test.npy')
-    y_test = np.load('dataset/yeast/y_test.npy')
+def load_data(dataset_name):
+    x_train = np.load('../dataset/' + dataset_name + '/x_train.npy')
+    y_train = np.load('../dataset/' + dataset_name + '/y_train.npy')
+    x_test = np.load('../dataset/' + dataset_name + '/x_test.npy')
+    y_test = np.load('../dataset/' + dataset_name + '/y_test.npy')
     x_train, y_train = eliminate_data(x_train, y_train)
 
     return x_train, y_train, x_test, y_test
@@ -145,5 +148,6 @@ def eliminate_data(data_x, data_y):
     return data_x, data_y
 
 if __name__ == '__main__':
-    x_train, y_train, _, _ = load_data()
+    x_train, y_train, _, _ = load_data('yeast')
     train(x_train, y_train)
+    #train(x_train, y_train)
